9fc4323fddfd9b604b82e85e1ae8e53aea194525
Suleman Khan <sulemank97@gmail.com>
2022-05-20 14:38:58 +0500

    feat: add color and font theme configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,13 @@ import Logo from 'public/icons/logo.svg';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-dark">
       <Logo />
+      <h1 className="text-light">Examine</h1>
+      <h2 className="text-light">Examine</h2>
+      <h3 className="text-light">Examine</h3>
+      <h4 className="text-light">Examine</h4>
+      <h5 className="text-light">Examine</h5>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,44 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#5E3B76',
+        secondary: '#25A1AF',
+        tertiary: '#6C757D',
+        info: '#E2D9F3',
+        danger: '#DC3545',
+        light: '#F8F9FA',
+        caution: '#FDEFEF',
+        premium: '#FFC107',
+        dark: '#212529',
+      },
+      fontFamily: {
+        serif: ['Lora', ...defaultTheme.fontFamily.serif],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        'mobile-h1': ['36px', { lineHeight: '48px' }],
+        'mobile-h2': ['20px', { lineHeight: '25px' }],
+        'mobile-h3': ['16px', { lineHeight: '20px' }],
+        'mobile-h4': ['12px', { lineHeight: '16px' }],
+        'web-h1': ['60px', { lineHeight: '75px' }],
+        'web-h2': ['40px', { lineHeight: '55px' }],
+        'web-h3': ['30px', { lineHeight: '36px' }],
+        'web-h4': ['25px', { lineHeight: '32px' }],
+        'web-h5': ['22px', { lineHeight: '28px' }],
+        'web-h6': ['18px', { lineHeight: '27px' }],
+        'mobile-p1': ['18px', { lineHeight: '36px' }],
+        'mobile-p2': ['16px', { lineHeight: '28px' }],
+        'mobile-p3': ['14px', { lineHeight: '20px' }],
+        'web-p1': ['20px', { lineHeight: '36px' }],
+        'web-p2': ['18px', { lineHeight: '30px' }],
+        'web-p3': ['16px', { lineHeight: '26px' }],
+        'web-p4': ['14px', { lineHeight: '22px' }],
+      },
+    },
   },
   plugins: [],
 };
