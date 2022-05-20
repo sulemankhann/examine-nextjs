35abd8e595837cffe20f62870b9e7892d140b70a
Suleman Khan <sulemank97@gmail.com>
2022-05-20 11:21:38 +0500

    Initialze project with NextJS, Typescript and TailwindCSS

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import Logo from 'public/icons/logo.svg';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+};
+
+export default Home;
